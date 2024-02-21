755b9be64aa55f5f45f798fade4c50d21d593cdf
Andre Morryson <amorrysson77@gmail.com>
2024-02-21 19:59:26 -0300

    add logo and input, add css for input

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,8 +1,18 @@
+import logo from './assets/Logo.svg'
+
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="mx-auto max-w-6xl my-12 ">
+      <img src={logo} alt='Nlw Expert' />
+    
+      <form className="w-full">
+        <input 
+          type='text' 
+          placeholder='Busce em suas notas...'
+          className="w-full bg-transparent text-3xl font-semibold outline-none tracking-tight placeholder:text-slate-500 " 
+        />
+      </form>
+    </div>
   )
 }
 
