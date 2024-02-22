8f64d01c894d58b8fe4830922388052c7b402893
Andre Morryson <amorrysson77@gmail.com>
2024-02-21 21:22:13 -0300

    create new note card and note card and style each one

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,17 +1,27 @@
 import logo from './assets/Logo.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
 
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12 ">
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt='Nlw Expert' />
     
-      <form className="w-full">
+      <form className="w-full ">
         <input 
           type='text' 
           placeholder='Busce em suas notas...'
           className="w-full bg-transparent text-3xl font-semibold outline-none tracking-tight placeholder:text-slate-500 " 
         />
       </form>
+
+      <div className='h-px bg-slate-700'/>
+
+      <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
+        <NewNoteCard/>
+        <NoteCard/>
+        <NoteCard/>
+      </div>
     </div>
   )
 }
---
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -0,0 +1,16 @@
+export function NoteCard() {
+  return (  
+  <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+        <span className='text-sm font-medium text-slate-300'>
+          há 2 dias
+        </span>
+
+        <p className='text-sm leading-6 text-slate-400'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur quo laudantium dolore, odio harum veniam ea neque voluptatum ipsum vitae non voluptate rem esse, sint repellat tempore quisquam modi obcaecati.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur quo laudantium dolore, odio harum veniam ea neque voluptatum ipsum vitae non voluptate rem esse, sint repellat tempore quisquam modi obcaecati.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur quo laudantium dolore, odio harum veniam ea neque voluptatum ipsum vitae non voluptate rem esse, sint repellat tempore quisquam modi obcaecati.
+        </p>
+      <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
+  </div>
+  )
+}
