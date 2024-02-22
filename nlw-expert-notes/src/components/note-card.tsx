3f04fa1e2412724a80eb91d711ab14d32711e02b
Andre Morryson <amorrysson77@gmail.com>
2024-02-21 21:50:36 -0300

    adjust at focus and hover at cards

--- nlw-expert-notes/src/components/note-card.tsx
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
   return (  
-  <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+  <button className='rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-slate-600 hover:ring-2 focus-visible:ring-2 focus-visible:ring-lime-400'>
         <span className='text-sm font-medium text-slate-300'>
           há 2 dias
         </span>
@@ -11,6 +11,6 @@ export function NoteCard() {
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur quo laudantium dolore, odio harum veniam ea neque voluptatum ipsum vitae non voluptate rem esse, sint repellat tempore quisquam modi obcaecati.
         </p>
       <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
-  </div>
+  </button>
   )
 }
